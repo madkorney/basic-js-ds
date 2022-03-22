5d3005b1ed75805845b1eaaa984210296f66264a
Sergey Kornienko <madkorney@gmail.com>
2022-03-22 20:48:18 +0300

    feat: implement queue class methods

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,22 +13,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+ 
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.UnderlyingList;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    
+
+    if (!this.UnderlyingList) {
+      this.UnderlyingList = new ListNode(value)
+    } else {
+
+
+    let lastNode = this.UnderlyingList;
+    
+      while (lastNode.next) {
+        lastNode = lastNode.next
+      }
+      lastNode.next = new ListNode(value);
+    }
+    
+    
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let elementFromQueue = this.UnderlyingList.value; 
+    this.UnderlyingList = this.UnderlyingList.next;
+    
+    // this.UnderlyingList.value = this.UnderlyingList.next.value;
+    // this.UnderlyingList.next = this.UnderlyingList.next.next;
+
+    return elementFromQueue
   }
 }
+
+// const queue = new Queue();
+// console.log( queue.getUnderlyingList());
+
+// queue.enqueue(1); // adds the element to the queue
+// console.log( queue.getUnderlyingList());
+//  queue.enqueue(3); // adds the element to the queue
+//  console.log( queue.getUnderlyingList());
+
+//  queue.enqueue(33); // adds the element to the queue
+//  console.log( queue.getUnderlyingList());
+
+//  console.log(queue.dequeue()); // returns the top element from queue and deletes it, returns 1
+//  console.log( queue.getUnderlyingList());
+
+//  queue.enqueue(99); // adds the element to the queue
+//  console.log( queue.getUnderlyingList());
+
+//  console.log(queue.dequeue());
+//  console.log( queue.getUnderlyingList()); // returns { value: 3, next: null }
+
+
+
 
 module.exports = {
   Queue
